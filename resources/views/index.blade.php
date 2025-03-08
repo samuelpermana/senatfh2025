3da7ddc2e51625f0a2df1a745f9eaae34b352248
RaoulPurba <raoulpurba@gmail.com>
2025-03-08 23:08:50 +0700

    Styling Berita Index pt1

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,8 +20,7 @@
   <link href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css' rel='stylesheet'>
   <link href="https://cdnjs.cloudflare.com/ajax/libs/izitoast/1.4.0/css/iziToast.min.css" rel="stylesheet" integrity="sha512-O03ntXoVqaGUTAeAmvQ2YSzkCvclZEcPQu1eqloPaHfJ5RuNGiS4l+3duaidD801P50J28EHyonCV06CUlTSag==" crossorigin="anonymous"
     referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
 
   <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css" rel="stylesheet" integrity="sha512-mSYUmp1HYZDFaVKK//63EcZq4iFWFjxSL+Z3T/aCt4IO9Cejm03q3NKKYN6pFQzY0SBOr8h+eCIAZHPXcpZaNw=="
     crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -40,25 +39,68 @@
     <div class="blob-bg">
     </div>
   </div>
+  {{-- Ini buatan rajen --}}
 
-  <section class="latest-news">
+  {{-- <section class="latest-news">
     <h2>Latest Berita</h2>
     <div class="news-container">
-      @foreach(App\Models\Berita::latest()->take(3)->get() as $berita)
+      @foreach (App\Models\Berita::latest()->take(3)->get() as $berita)
         <div class="news-item">
           <h3><a href="/beritapublic/{{ $berita->id }}">{{ $berita->title }}</a></h3>
           <p>{{ Str::limit($berita->content, 100) }}</p>
-          <p><i class="fas fa-calendar-alt"></i> {{ $berita->created_at->format('d F Y') }}</p>
-          @if($berita->picture)
-              <img src="{{ asset('storage/' . $berita->picture) }}" alt="Picture" style="max-width: 100px;">
+          <p><i class="fas fa-calendar-alt"></i> {{ $berita->created_at->format("d F Y") }}</p>
+          @if ($berita->picture)
+            <img src="{{ asset("storage/" . $berita->picture) }}" alt="Picture" style="max-width: 100px;">
           @endif
         </div>
       @endforeach
     </div>
-    <a href="{{ url('/beritamore') }}" class="btn">More Berita</a>
+    <a class="btn" href="{{ url("/beritamore") }}">More Berita</a>
+  </section> --}}
+
+  {{-- Akhir dari buatan rajen --}}
+
+  <section class="container9" class="latest-news">
+    <div class="flex">
+      <!-- Main News Section -->
+      <div class="main-news">
+        <div class="slider">
+          <button class="nav-button left" onclick="prevSlide()"><i class="fas fa-chevron-left"></i></button>
+          <div class="slides">
+            @foreach (App\Models\Berita::latest()->take(3)->get() as $berita)
+              <div class="slide">
+                <img src="{{ asset("storage/" . $berita->picture) }}" alt="Picture">
+                <div class="info">
+                  <span class="tag">Latest Berita</span>
+                  <h2><a class="berita_title" href="/beritapublic/{{ $berita->id }}">{{ $berita->title }}</a></h2>
+                  <p><i class="fas fa-calendar-alt"></i> {{ $berita->created_at->format("d F Y") }}</p>
+                </div>
+              </div>
+            @endforeach
+          </div>
+          <button class="nav-button right" onclick="nextSlide()"><i class="fas fa-chevron-right"></i></button>
+        </div>
+      </div>
+
+      <!-- Popular News Section -->
+      <div class="div-popular-news">
+        <div class="popular-news">
+          <h2>HOT NEWS</h2>
+
+          @foreach (App\Models\Berita::latest()->take(3)->get() as $berita)
+            <div class="news-item">
+              <img src="{{ asset("storage/" . $berita->picture) }}" alt="Picture" style="max-width: 100px;">
+              <div class="info">
+                <h3><a class="berita_title" href="/beritapublic/{{ $berita->id }}" style="color:black">{{ $berita->title }}</a></h3>
+                <p><i class="fas fa-calendar-alt"></i> {{ $berita->created_at->format("d F Y") }}</p>
+              </div>
+            </div>
+          @endforeach
+        </div>
+        <br><a class="btn" href="{{ url("/beritamore") }}">More Berita</a>
+      </div>
+    </div>
   </section>
-
-
 
   <header class="container">
     <div class="content">
@@ -67,23 +109,24 @@
       <h4 style="font-size: 2rem;">SENAT MAHASISWA FAKULTAS HUKUM UNIVERSITAS DIPONEGORO</h4>
       <h1 style="color:#2B5C6B;">GEMILANG PEMRAKARSA</h1>
       <p>
-        Selamat datang di portal resmi Senat Fakultas Hukum Universitas Diponegoro! Kami bangga menjadi wadah bagi mahasiswa-mahasiswa FH Undip untuk berpartisipasi aktif dalam proses pembelajaran, pengembangan diri, serta pengabdian kepada mahasiswa Fakultas Hukum Universitas Diponegoro.
-        Sebagai lembaga yang mewakili suara mahasiswa, Senat FH Undip berkomitmen untuk menjaga dan memajukan kualitas kinerja di lingkungan fakultas. Kami mengedepankan nilai-nilai humanis, profesional, dan berwibawa dalam setiap langkah yang kami ambil.
-        Melalui situs web ini, Anda akan menemukan informasi terkini mengenai kegiatan, program, dan berbagai kegiatan yang kami selenggarakan. Kami juga mengundang Anda untuk terlibat secara aktif dalam berbagai kegiatan yang kami adakan, serta menyampaikan aspirasi dan gagasan Anda untuk kemajuan bersama.
+        Selamat datang di portal resmi Senat Fakultas Hukum Universitas Diponegoro! Kami bangga menjadi wadah bagi mahasiswa-mahasiswa FH Undip untuk berpartisipasi aktif dalam proses pembelajaran, pengembangan diri, serta pengabdian kepada
+        mahasiswa Fakultas Hukum Universitas Diponegoro.
+        Sebagai lembaga yang mewakili suara mahasiswa, Senat FH Undip berkomitmen untuk menjaga dan memajukan kualitas kinerja di lingkungan fakultas. Kami mengedepankan nilai-nilai humanis, profesional, dan berwibawa dalam setiap langkah
+        yang kami ambil.
+        Melalui situs web ini, Anda akan menemukan informasi terkini mengenai kegiatan, program, dan berbagai kegiatan yang kami selenggarakan. Kami juga mengundang Anda untuk terlibat secara aktif dalam berbagai kegiatan yang kami adakan,
+        serta menyampaikan aspirasi dan gagasan Anda untuk kemajuan bersama.
         Terima kasih telah mengunjungi situs web Senat FH Undip. Mari bersama-sama menciptakan lingkungan belajar yang inspiratif dan berdaya saing tinggi bagi mahasiswa FH Undip.Viva Legislativa!
       </p>
-      <a class="btn" href="{{ url('/selayangpandang#makna-logo') }}">Makna Logo</a>
+      <a class="btn" href="{{ url("/selayangpandang#makna-logo") }}">Makna Logo</a>
     </div>
     <div class="logo-container">
       <img src="img/kabinetridho.png" alt="Logo Api">
-      
-  </div>
-  
-  </div>
-  
-  </header>
 
-  
+    </div>
+
+    </div>
+
+  </header>
 
   <section class="container">
     <h2 class="header">AKTIVITAS LEGISLATIF</h2>
@@ -114,14 +157,14 @@
       const modal = $('#modal-action')
       const csrfToken = $('meta[name=csrf_token]').attr('content')
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-        initialView: 'dayGridMonth',
-        themeSystem: 'bootstrap5',
-        events: "{{ route("legislasi.list") }}",
-        editable: false,
-        eventDidMount: function(info) {
+      document.addEventListener('DOMContentLoaded', function() {
+        var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+          initialView: 'dayGridMonth',
+          themeSystem: 'bootstrap5',
+          events: "{{ route("legislasi.list") }}",
+          editable: false,
+          eventDidMount: function(info) {
             // Memeriksa kategori dari event
             var category = info.event.extendedProps.category;
 
@@ -130,45 +173,45 @@
             var borderColor;
 
             switch (category) {
-                case 'success':
-                    backgroundColor = '#2B6172';
-                    borderColor = '#2B6172';
-                    break;
-                case 'danger':
-                    backgroundColor = '#FDF4E6';
-                    borderColor = '#FDF4E6';
-                    break;
-                case 'warning':
-                    backgroundColor = '#FEFEFE';
-                    borderColor = '#FEFEFE';
-                    break;
-                case 'info':
-                    backgroundColor = '#AEAAAA';
-                    borderColor = '#AEAAAA';
-                    break;
-                case 'primary':
-                    backgroundColor = '#A13D3E';
-                    borderColor = '#A13D3E';
-                    break;
-                case 'secondary':
-                    backgroundColor = '#94e51a';
-                    borderColor = '#94e51a';
-                    break;
-                case 'dark':
-                    backgroundColor = '#8e7e71';
-                    borderColor = '#8e7e71';
-                    break; 
-                case 'light':
-                    backgroundColor = '#96a659';
-                    borderColor = '#96a659';
-                    break;
-                case 'link':
-                    backgroundColor = '#84629d';
-                    borderColor = '#84629d';
-                    break;
-                default:
-                    backgroundColor = '#CCCCCC';
-                    borderColor = '#CCCCCC';
+              case 'success':
+                backgroundColor = '#2B6172';
+                borderColor = '#2B6172';
+                break;
+              case 'danger':
+                backgroundColor = '#FDF4E6';
+                borderColor = '#FDF4E6';
+                break;
+              case 'warning':
+                backgroundColor = '#FEFEFE';
+                borderColor = '#FEFEFE';
+                break;
+              case 'info':
+                backgroundColor = '#AEAAAA';
+                borderColor = '#AEAAAA';
+                break;
+              case 'primary':
+                backgroundColor = '#A13D3E';
+                borderColor = '#A13D3E';
+                break;
+              case 'secondary':
+                backgroundColor = '#94e51a';
+                borderColor = '#94e51a';
+                break;
+              case 'dark':
+                backgroundColor = '#8e7e71';
+                borderColor = '#8e7e71';
+                break;
+              case 'light':
+                backgroundColor = '#96a659';
+                borderColor = '#96a659';
+                break;
+              case 'link':
+                backgroundColor = '#84629d';
+                borderColor = '#84629d';
+                break;
+              default:
+                backgroundColor = '#CCCCCC';
+                borderColor = '#CCCCCC';
             }
 
             // Atur warna background dan border
@@ -176,31 +219,30 @@
             info.el.style.borderColor = borderColor;
             const title = info.event.title;
             info.el.setAttribute('title', title);
-        },
-        eventClick: function(arg) {
-            var title = arg.event.title; 
+          },
+          eventClick: function(arg) {
+            var title = arg.event.title;
             const eventDetailHTML = `
                 <div>
                     <h2>${title}</h2>
                 </div>
             `;
             modal.html(eventDetailHTML).modal('show');
-        }
-    });
-    calendar.render();
-});
+          }
+        });
+        calendar.render();
+      });
 
-document.addEventListener('DOMContentLoaded', function() {
-    setTimeout(() => {
-        document.querySelectorAll('.fc-day-today').forEach(el => {
+      document.addEventListener('DOMContentLoaded', function() {
+        setTimeout(() => {
+          document.querySelectorAll('.fc-day-today').forEach(el => {
             el.style.backgroundColor = "#2B5C6B";
 
             el.style.color = "#2B5C6B";
 
-        });
-    }, 500);
-});
-
+          });
+        }, 500);
+      });
     </script>
 
   </section>
@@ -208,53 +250,58 @@ document.addEventListener('DOMContentLoaded', function() {
   <script>
     const showMenu = (headerToggle, navbarId) => {
       const toggleBtn = document.getElementById(headerToggle),
-        nav = document.getElementById(navbarId)
+        nav = document.getElementById(navbarId);
 
       // Validate that variables exist
-      if (headerToggle && navbarId) {
+      if (toggleBtn && nav) {
         toggleBtn.addEventListener('click', () => {
-          // We add the show-menu class to the div tag with the nav__menu class
-          nav.classList.toggle('show-menu')
-          // change icon
-          toggleBtn.classList.toggle('bx-x')
-        })
+          // Toggle the show-menu class
+          nav.classList.toggle('show-menu');
+          // Change icon
+          toggleBtn.classList.toggle('bx-x');
+        });
       }
-    }
-    showMenu('header-toggle', 'navbar')
+    };
+    showMenu('header-toggle', 'navbar');
 
     /*==================== LINK ACTIVE ====================*/
-    const linkColor = document.querySelectorAll('.nav-links')
+    const linkColor = document.querySelectorAll('.nav-links');
 
     function colorLink() {
-      linkColor.forEach(l => l.classList.remove('active'))
-      this.classList.add('active')
+      linkColor.forEach(l => l.classList.remove('active'));
+      this.classList.add('active');
     }
+
+    linkColor.forEach(l => l.addEventListener('click', colorLink));
 
     let currentIndex = 0;
     const slides = document.querySelectorAll('.slide');
-    
+
     function showSlide(index) {
-        slides.forEach((slide, i) => {
-            slide.classList.toggle('active', i === index);
-        });
+      slides.forEach((slide, i) => {
+        slide.classList.toggle('active', i === index);
+      });
     }
-    
+
     function nextSlide() {
-        currentIndex = (currentIndex + 1) % slides.length;
-        showSlide(currentIndex);
+      currentIndex = (currentIndex + 1) % slides.length;
+      showSlide(currentIndex);
     }
-    
+
     function prevSlide() {
-        currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-        showSlide(currentIndex);
+      currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+      showSlide(currentIndex);
     }
-    
 
+    // Ensure no slides have "active" class initially
+    slides.forEach(slide => slide.classList.remove('active'));
 
-
+    // Optionally, activate the first slide dynamically
+    if (slides.length > 0) {
+      showSlide(0);
+    }
   </script>
+
   <script src="js-aktivitas-legislasi.js"></script>
   <script src="js-aktivitas-sm-fh.js"></script>
-
-
 @endsection
