8095eb1c6a507183b8209cd6d5140ef307b86833
Farrel Belva <farrelirwansyah02@gmail.com>
2025-06-07 21:23:46 +0700

    link peminjaman ruangan

--- resources/views/peminjamanruangan.blade.php
+++ resources/views/peminjamanruangan.blade.php
@@ -70,7 +70,7 @@
         <h2 class="header">TAMPILAN EXCEL</h2>
         <p class="sub-header">Berisikan tentang data ruangan yang meminjam tempat di FH dalam bentuk exel</p>
         <main class="table4" id="customers_table">
-          <a href="https://bit.ly/PeminjamanRuanganFakultasHukumUndip">Link Tampilan Excel</a>
+          <a href="https://docs.google.com/spreadsheets/d/1nihgrk4kaxd9PXdV1AcGlUIzPX78aVp4FnBBOjjdVSE/edit?usp=drivesdk">Link Tampilan Excel</a>
         </main>
       </section>
 
